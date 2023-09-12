3d9332866cf514b54c925d76b9c01cfd4a7ddcca
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-12 17:11:45 +0700

    add logic for swicthing tab

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 import displayNav from "./nav";
 import displayHome from "./home";
+import displayMenu from "./menu";
 
-displayNav()
-displayHome()
+
+const start = () => {
+  displayNav();
+  // displayHome();
+  const homeBtn = document.querySelector(".home");
+  const menuBtn = document.querySelector('.menu')
+  const content = document.getElementById("content");
+  
+  const resetDisplay = () => {
+    content.innerHTML = "";
+  };
+  
+  homeBtn.addEventListener("click", () => {
+    resetDisplay();
+    displayNav();
+    displayHome();
+    start();
+  });
+  
+  menuBtn.addEventListener('click', () => {
+    resetDisplay();
+    displayNav();
+    displayMenu();
+    start();
+  })
+}
+
+start()
