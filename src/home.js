1ce06e6e48132c48dac9a168b08127a20d190d4c
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-12 17:10:01 +0700

    remove unused variable

--- src/home.js
+++ src/home.js
@@ -3,7 +3,6 @@ export default function displayHome() {
   const main = document.createElement('main')
   const container = document.createElement('div')
   const h1 = document.createElement('h1')
-  const br = document.createElement('br')
   const p = document.createElement('p')
   p.innerText = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, sunt! 
   consectetur adipisicing elit. Qui, sunt!`
