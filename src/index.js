6adeab14c8a04fd37cf8ed290bfa288855600419
sevaaaDev <136947471+sevaaaDev@users.noreply.github.com>
2023-09-13 15:23:04 +0700

    add contact to the logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import displayNav from "./nav";
 import displayHome from "./home";
 import displayMenu from "./menu";
+import displayContact from "./contact";
 
 displayNav();
 displayHome();
@@ -9,6 +10,7 @@ const start = () => {
   // displayHome();
   const homeBtn = document.querySelector(".home");
   const menuBtn = document.querySelector('.menu')
+  const contactBtn = document.querySelector('.contact')
   const content = document.getElementById("content");
   
   const resetDisplay = () => {
@@ -26,6 +28,13 @@ const start = () => {
     resetDisplay();
     displayNav();
     displayMenu();
+    start();
+  })
+
+  contactBtn.addEventListener('click', () => {
+    resetDisplay();
+    displayNav();
+    displayContact();
     start();
   })
 }
